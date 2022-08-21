35b841b2cb1d71d9b6d66bb5cdddeee7084b5e07
MariaGee <masha.avramchencko@yandex.com>
2022-08-21 12:35:17 +0300

    Add Example 029

---
+++ Example029/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]        6, 1, 33 -> [6, 1, 33]
+
+int[] numbers = new int[8];
+
+int[] EnteringArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 10);
+    }
+    return arr;
+}
+
+int[] ArrayOutput(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + "  ");
+    }
+    return array;
+}
+
+numbers = EnteringArray(numbers);
+Console.WriteLine("Был задан массив: ");
+numbers = ArrayOutput(numbers);
+
